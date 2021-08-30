d2fa228eda2ef548a01d5c54a3c4602160611a0e
jumdemonali <jumdemonali@gmail.com>
2021-08-30 09:29:36 +0530

    Added: Tic Tac Toe board

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -1,6 +1,14 @@
 public class TicTacToe {
-    public static void main(String args[])
-    {
+    static char[] board = new char[10];
+    public static void main(String args[]) {
         System.out.println("Welcome to Tic-Tac-Toe game");
+        board();
+    }
+
+    public static void board() {
+
+        for (int i = 1; i < board.length; i++) {
+            board[i] = ' ';
+        }
     }
 }
