4ab1275fd77c488630b3526c233f35d1de9eca09
jumdemonali <jumdemonali@gmail.com>
2021-08-30 15:41:23 +0530

    Modified : Method showBoard

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -34,11 +34,11 @@ public class TicTacToe {
 
     public static void showBoard() {
         System.out.println(" ----------- ");
-        System.out.println("| " + board[0] + " | " + board[1] + " | " + board[2] + " |");
+        System.out.println("| " + board[1] + " | " + board[2] + " | " + board[3] + " |");
         System.out.println("|-----------|");
-        System.out.println("| " + board[3] + " | " + board[4] + " | " + board[5] + " |");
+        System.out.println("| " + board[4] + " | " + board[5] + " | " + board[6] + " |");
         System.out.println("|-----------|");
-        System.out.println("| " + board[6] + " | " + board[7] + " | " + board[8] + " |");
+        System.out.println("| " + board[7] + " | " + board[8] + " | " + board[9] + " |");
         System.out.println(" ----------- ");
     }
 }
