ae13389984041e9af975d07887e87d82af072496
jumdemonali <jumdemonali@gmail.com>
2021-08-30 10:17:09 +0530

    Added: Player chosing X or O

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -1,14 +1,32 @@
+import java.util.Scanner;
+
 public class TicTacToe {
     static char[] board = new char[10];
+
     public static void main(String args[]) {
         System.out.println("Welcome to Tic-Tac-Toe game");
         board();
+        PlayerOption();
     }
 
-    public static void board() {
-
+    private static void board() {
         for (int i = 1; i < board.length; i++) {
             board[i] = ' ';
+        }
+    }
+
+    public static void PlayerOption() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Player select X or O: ");
+        char playmakers = sc.next().charAt(0);
+        if (playmakers == 'O' || playmakers == 'X') {
+            System.out.println("Valid Selection");
+            if (playmakers == 'O') {
+                playmakers = 'X';
+            } else
+                playmakers = 'O';
+        } else {
+            System.out.println("Invalid Selection");
         }
     }
 }
