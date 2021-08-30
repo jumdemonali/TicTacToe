d3938f7283b984b892c78552f5bc042aee2d7e5c
jumdemonali <jumdemonali@gmail.com>
2021-08-30 10:48:18 +0530

    Modified: Player's choice

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -2,30 +2,33 @@ import java.util.Scanner;
 
 public class TicTacToe {
     static char[] board = new char[10];
-
     public static void main(String args[]) {
         System.out.println("Welcome to Tic-Tac-Toe game");
         board();
         PlayerOption();
     }
-
     private static void board() {
         for (int i = 1; i < board.length; i++) {
             board[i] = ' ';
         }
     }
-
     public static void PlayerOption() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Player select X or O: ");
-        char playmakers = sc.next().charAt(0);
-        if (playmakers == 'O' || playmakers == 'X') {
+        System.out.println("Player should select X or O: ");
+        char playerschoice = sc.next().charAt(0);
+        char computerchoice;
+        if(playerschoice == 'O'|| playerschoice =='X')
+        {
             System.out.println("Valid Selection");
-            if (playmakers == 'O') {
-                playmakers = 'X';
-            } else
-                playmakers = 'O';
-        } else {
+            if (playerschoice=='O')
+            {
+                computerchoice = 'X';
+            }
+            else
+                computerchoice='O';
+        }
+        else
+        {
             System.out.println("Invalid Selection");
         }
     }
