dab13d0807098a170912d17f5697a706693665cb
jumdemonali <jumdemonali@gmail.com>
2021-08-30 14:56:32 +0530

    Added : Method showBoard to display current board

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -2,34 +2,43 @@ import java.util.Scanner;
 
 public class TicTacToe {
     static char[] board = new char[10];
+
     public static void main(String args[]) {
         System.out.println("Welcome to Tic-Tac-Toe game");
-        board();
-        PlayerOption();
+        boardInit();
+        playerOption();
+        showBoard();
     }
-    private static void board() {
+
+    private static void boardInit() {
         for (int i = 1; i < board.length; i++) {
             board[i] = ' ';
         }
     }
-    public static void PlayerOption() {
+
+    public static void playerOption() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Player should select X or O: ");
         char playerschoice = sc.next().charAt(0);
         char computerchoice;
-        if(playerschoice == 'O'|| playerschoice =='x')
-        {
+        if (playerschoice == 'O' || playerschoice == 'X') {
             System.out.println("Valid Selection");
-            if (playerschoice=='O')
-            {
+            if (playerschoice == 'O') {
                 computerchoice = 'X';
-            }
-            else
-                computerchoice='O';
-        }
-        else
-        {
+            } else
+                computerchoice = 'O';
+        } else {
             System.out.println("Invalid Selection");
         }
+    }
+
+    public static void showBoard() {
+        System.out.println(" ----------- ");
+        System.out.println("| " + board[0] + " | " + board[1] + " | " + board[2] + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + board[3] + " | " + board[4] + " | " + board[5] + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + board[6] + " | " + board[7] + " | " + board[8] + " |");
+        System.out.println(" ----------- ");
     }
 }
